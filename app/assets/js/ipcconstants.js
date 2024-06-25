5c703eba4c56d5dc7ae91819ade19cbb66bba063
S0KAA <173813774+S0KAA@users.noreply.github.com>
2024-06-25 19:33:13 +0200

    AZURE Client ID added

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '78997707-0ea5-4355-b71e-dde6893b8da2'
 // SEE NOTE ABOVE.
 
 
